d4ee203309fd89c7b25db49f353de40f195984ba
Yator1 <titusyatz17@gmail.com>
2024-04-22 12:19:45 +0300

    pycode style correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,42 +10,42 @@ import sys
 
 
 def get_todo(employee_id):
-        """
-        Returns TODO list for an Employee.
-        :param employee_id: The employee's ID.
-        """
+    """
+    Returns TODO list for an Employee.
+    :param employee_id: The employee's ID.
+    """
 
-        base_url = 'https://jsonplaceholder.typicode.com'
+    base_url = 'https://jsonplaceholder.typicode.com'
 
-        employee_resp = requests.get(f"{base_url}/users/{employee_id}")
-        if employee_resp.status_code == 200:
-            employee_data = employee_resp.json()
-            employee_name = employee_data["name"]
-        else:
-            return
+    employee_resp = requests.get(f"{base_url}/users/{employee_id}")
+    if employee_resp.status_code == 200:
+        employee_data = employee_resp.json()
+        employee_name = employee_data["name"]
+    else:
+        return
 
-        todo_resp = requests.get(f"{base_url}/todos?userId={employee_id}")
+    todo_resp = requests.get(f"{base_url}/todos?userId={employee_id}")
 
-        if todo_resp.status_code == 200:
-            todo_data = todo_resp.json()
-        else:
-            return
+    if todo_resp.status_code == 200:
+        todo_data = todo_resp.json()
+    else:
+        return
 
-        total_tasks = len(todo_data)
-        completed_tasks = sum(1 for task in todo_data if task["completed"])
+    total_tasks = len(todo_data)
+    completed_tasks = sum(1 for task in todo_data if task["completed"])
 
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee_name,
-            completed_tasks,
-            total_tasks)
-            )
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name,
+        completed_tasks,
+        total_tasks)
+        )
 
-        completed = []
-        for task in todo_data:
-            if task["completed"]:
-                completed.append(task)
-        for task in completed:
-            print("\t {}".format(task["title"]))
+    completed = []
+    for task in todo_data:
+        if task["completed"]:
+            completed.append(task)
+    for task in completed:
+        print("\t {}".format(task["title"]))
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
